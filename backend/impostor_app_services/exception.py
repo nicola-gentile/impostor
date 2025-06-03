65aac641361f070f96ae68e9dc276eed13f33d51
nicola gentile <nicola.gentile.developer@gmail.com>
2025-06-03 19:45:54 +0200

    remove useless file

--- backend/impostor_app_services/exception.py
+++
@@ -1,7 +0,0 @@
-
-class RoomClosedError(Exception):
-    """Exception raised when trying to perform an action on a closed room."""
-    def __init__(self, message="The room is closed.", session=None):
-        self.message = message
-        self.session = session
-        super().__init__(self.message)
