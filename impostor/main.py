ce92db272b7d6b06cb0ae7aaaa64fb33b44018b9
nicola gentile <nicola.gentile.developer@gmail.com>
2025-06-11 14:25:25 +0200

    add log to clean_room

--- impostor/main.py
+++ impostor/main.py
@@ -39,6 +39,7 @@ async def room_all():
 
 def clean_room(room_id: int):
     if sse.is_alive(room_id):
+        print(f'cleaning room {room_id}')
         sse.set_alive(room_id, False)
         with Session(db.engine) as session:
             room = query.room_get(room_id, session)
